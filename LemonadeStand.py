d44a2c244672bf5da52fd1adbc651c3c05d3dfd0
KylefromWI <42153739+KylefromWI@users.noreply.github.com>
2025-07-25 10:28:47 -0500

    add secret menu

--- LemonadeStand.py
+++ LemonadeStand.py
@@ -5,13 +5,34 @@ import time
 done = False
 lemonadeCost = 30.00 #lemonade costs a lot in this world
 breadCost = 1.00
+beanCost = 2.00
 userMoney = 260 #user has a lot of cash
+toeCost = 50.0
+kidneyCost = 100.00
 
 #Creates a function that prints how much money the user has
 def printMoney():
     cash = str(userMoney)
     print("You have $", cash, "left")
     time.sleep(2)
+
+
+def secretMenu():
+    
+    print("======================================")
+    print(" YOUVE REACHED THE SECRET MENU")
+    print("======================================")
+    print()
+
+    choice = input("You've reached the secret menu! Would you like a toe or a kidney?")
+    if choice == "a toe":
+        userMoney -= toeCost
+        print("Enjoy that toe, good luck")
+        printMoney()
+    elif choice == "a kidney":
+        userMoney -= kidneyCost
+        print("Get well soon")
+        printMoney()
 
 # Main loop
 while not done:
@@ -21,7 +42,7 @@ while not done:
     print("======================================")
     print()
 
-    choice = input("What would you like to buy? or are you done? lemonade or bread are good choices  ")
+    choice = input("What would you like to buy? or are you done? lemonade, bean, or bread are good choices  ")
     if choice == "lemonade":
         userMoney -= lemonadeCost
         print("Thank you, here you go")
@@ -30,11 +51,17 @@ while not done:
         userMoney -= breadCost
         print("Thank you, here is your bread")
         printMoney()
+    elif choice == "bean":
+        userMoney -= beanCost
+        print("Thank you, here is your bread")
+        printMoney()
+    elif choice == "secret menu":
+        secretMenu()sec
     elif choice == "done":
         done = True
         print("Thank you, good bye")
         printMoney()
     else:
-        print("I don't understand. You can buy lemonade, bread, or you can be done")
+        print("I don't understand. You can buy lemonade, bread, bean, or you can be done")
 
 print("Another satisifed customer!!")
