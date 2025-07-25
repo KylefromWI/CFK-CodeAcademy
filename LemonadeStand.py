e0a0b10f62ccc7bfb8e1744076f58cc47be111ee
KylefromWI <42153739+KylefromWI@users.noreply.github.com>
2025-07-25 09:31:41 -0500

    Update LemonadeStand.py

--- LemonadeStand.py
+++ LemonadeStand.py
@@ -1,9 +1,15 @@
+import time
 done = False
 
 lemonadeCost = 30.00
 breadCost = 1.00
 
 userMoney = 26000000000
+
+def printMoney():
+    cash = str(userMoney)
+    print("You have $", cash, "left")
+    time.sleep(2)
 
 while not done:
 
@@ -12,17 +18,20 @@ while not done:
     print("======================================")
     print()
 
-    choice = input("What would you like to buy? or are you done? lemonade or bread are good choices")
+    choice = input("What would you like to buy? or are you done? lemonade or bread are good choices  ")
     if choice == "lemonade":
         userMoney -= lemonadeCost
         print("Thank you, here you go")
+        printMoney()
     elif choice == "bread":
         userMoney -= breadCost
         print("Thank you, here is your bread")
-
+        printMoney()
     elif choice == "done":
         done = True
         print("Thank you, good bye")
+        printMoney()
     else:
-        cash = str(userMoney)
-        print("You have $", cash, "left")
+        print("I don't understand. You can buy lemonade, bread, or you can be done")
+
+print("Another satisifed customer!!")
